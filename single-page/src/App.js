bb1e27d709261ffe6fa09d9960cca025f3e57e7e
Collin Didier <collin.didier80@gmail.com>
2020-11-03 00:27:22 -0600

    [app.js] add remaining notes and content

--- single-page/src/App.js
+++ single-page/src/App.js
@@ -1,22 +1,31 @@
+// App.js is the 2nd highest javascript file in the tree hierarchy. Use it to assemble 
+// the structure and content of the application before exporting it to index.js for configuration
+// and rendering
+
+
 import React, { Component } from 'react'
 import './styles/App.css';
-import ComponentOne from 'ComponentOne'
-import ComponentTwo from 'ComponentTwo'
+import ComponentOne from './components/ComponentOne'
+import ComponentTwo from './components/ComponentTwo'
 
 
 class App extends Component {
-  constructor(){
-    super()
+  // inclusion of props depends on whether index.js passes any
+  constructor(props){
+    super(props)
     this.state = {
-        key: value
+        key: "example value",
+        key2: "2nd example value"
     }
   }
 
-  // Optional - set the initial state
+  // Optional - set the initial state or run any other code needed when the component finishes mounting
   // componentDidMount (){
   //   try {
   //       this.setState((prevState) => ({
-  //          // set initial state
+  //          // set initial state here
+  //          ({exampleKey : exampleValue})   
+         
   //       }))
   //   } catch (error) {
   //       console.log(error)
@@ -26,8 +35,8 @@ class App extends Component {
   render() {
     return (
     <main className="appContentWrapper">
-        <ComponentOne />
-        <ComponentTwo />
+        <ComponentOne className="example example1" exampleProp={this.state.key}/>
+        <ComponentTwo className="example example2" exampleProp={this.state.key2}/>
     </main>
     )}
   }
@@ -35,11 +44,9 @@ class App extends Component {
   export default App
 
 
-
-
 // Option 2: functional-component version 
 
-// function App() {
+// function App(props) {
 //   return (
 //     <main className="appContentWrapper">
 //         <ComponentOne />
