54e2dba5a87394aef2c6cc992bfbe33220d69d46
Collin Didier <collin.didier80@gmail.com>
2020-11-03 00:27:36 -0600

    [index.js] add notes

--- single-page/src/index.js
+++ single-page/src/index.js
@@ -1,6 +1,11 @@
+// index.js is the highest javascript file in the heirarchy tree. It sets the React mode and renders the app content
+// created and structured in App.js, which is the 2nd highest file in the tree. In other words, the role of index.js
+// can be thought of as a configuration file using the React Dom, connecting to html, and rendering the app as a whole, 
+// whereas App.js builds out the content and html/jsx structure of the app. 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
@@ -9,3 +14,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
+// initialize the app by entering command     >npm i   or     >npm install
+// start the app by entering command     >npm start     in project directory
