3af2f2f4d2730ecc9125d2c0d58ab9303ad26f63
Collin Didier <collin.didier80@gmail.com>
2020-11-03 00:28:08 -0600

    [ComponentOne.js] create content and notes

--- single-page/src/components/ComponentOne.js
+++ single-page/src/components/ComponentOne.js
@@ -3,18 +3,20 @@ import React, { Component } from 'react'
 
 
 class ComponentOne extends Component {
+    // inclusion of props depends on whether app.js passes any
     constructor(props){
       super(props)
       this.state = {
-          key: value
+          key: "value"
       }
     }
   
-    // Optional - set the initial state
+    
+ // Optional - set the initial state or run any other code needed when the component finishes mounting
     // componentDidMount (){
     //   try {
     //       this.setState((prevState) => ({
-    //          // set initial state
+    //          // set initial state here
     //       }))
     //   } catch (error) {
     //       console.log(error)
@@ -23,27 +25,26 @@ class ComponentOne extends Component {
   
     render() {
       return (
-      <main className="appContentWrapper">
-          <ComponentOne />
-          <ComponentTwo />
-      </main>
+        <header className="componentOneWrapper">
+            <h1>This is Component One</h1>
+            <p>Change the file and jsx/html content as needed</p>
+            <p>The example prop passed to Component 1: {this.props.exampleProp}</p>
+        </header>
       )}
     }
   
-    export default App
-  
-  
+    export default ComponentOne
   
   
   // Option 2: functional-component version 
   
-  // function App() {
-  //   return (
-  //     <main className="appContentWrapper">
-  //         <ComponentOne />
-  //         <ComponentTwo />
-  //     </main>
+  // function ComponentOne(props) {
+    // return (
+    //     <header className="componentOneWrapper">
+    //         <h1>This is Component One</h1>
+    //         <p>Change the file and jsx/html content as needed</p>
+    //     </header>
   //   )
   // }
   
-  // export default App;
+  // export default ComponentOne;
